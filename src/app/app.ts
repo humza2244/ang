e62e48e4734c95c12bf0b5a92b9e3f945abe1e36
Humza Baig <62724748+humza2244@users.noreply.github.com>
2026-01-22 02:15:10 -0500

    Added working listing cards
    Added working listing cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './header/header';
-import { HomeItems } from "./home-items/home-items";
+import { Header } from './components/header/header';
+import { ListingCard } from './components/listing-card/listing-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, HomeItems],
+  imports: [RouterOutlet, Header, ListingCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
